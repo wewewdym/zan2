b410970a67e103c86743de2885edd0def1283158
MacBook-Pro-Kristina <kristina_macbook@icloud.com>
2023-12-01 11:37:45 +0300

    maxiprog.py changes

--- maxiprog.py
+++ maxiprog.py
@@ -10,5 +10,11 @@ max_value = num # обновляем максимальное значение
 
 return max_value
 
-input_list = [3, 8, 1, 6, 2, 7, 4, 5] # пример входного списка
+n=int(input())
+input_list =[]
+
+for i in range (n):
+num=int(input())
+input_list.append(num)
+
 print("Максимальное значение в списке:", find_maximum_value(input_list))
